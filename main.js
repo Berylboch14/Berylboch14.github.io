00d44089b2bbd92da175b4e9fc060fdd01b7142a
Berylboch14 <162893251+Berylboch14@users.noreply.github.com>
2024-04-08 18:59:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+// Add JavaScript code here
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav ul li a");
+    const headerText = document.querySelector(".header-text h1 span");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetSectionId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetSectionId);
+            const yOffset = -70;
+            const y = targetSection.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+            window.scrollTo({top: y, behavior: 'smooth'});
+        });
+    });
+
+    headerText.addEventListener("click", function() {
+        const yOffset = -70;
+        const y = document.getElementById("about").getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+        window.scrollTo({top: y, behavior: 'smooth'});
+    });
+});
 
