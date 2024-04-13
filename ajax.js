0cf103b54f84353c8b1577f05cd74e60c2147d3e
Berylboch14 <162893251+Berylboch14@users.noreply.github.com>
2024-04-13 19:09:20 +0800

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,10 @@
+function loadPage(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+}
